9b1d1cafae256721c85379e917d7d5d7a36c82f0
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2024-11-27 18:47:16 +1300

    range

--- wheel.js
+++ wheel.js
@@ -3,8 +3,9 @@
 const percentageText = document.querySelector('.percentage_text');
 const rangeSlider = document.querySelector('.range_slider');
 
-rangeSlider.addEventListener('mouseup', function() {
+rangeSlider.addEventListener('input', function() {
     let val = rangeSlider.value;
+    let valPercentage = (rangeSlider.value / rangeSlider.max) * 100;
     percentageText.textContent = `Percentage: ${val}%`  
+    rangeSlider.style.background = `linear-gradient(to right, #9381FF ${valPercentage}%, #fff ${valPercentage}%)`
 })
-
