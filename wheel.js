e395073129575e2f9057c9a8c1f2b897f65d605d
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2024-11-29 23:02:00 +1300

    attempt

--- wheel.js
+++ wheel.js
@@ -2,6 +2,7 @@
 
 const percentageText = document.querySelector('.percentage_text');
 const rangeSlider = document.querySelector('.range_slider');
+rangeSlider.style.background = `linear-gradient(to right, #9381FF ${50}%, #fff ${50}%)`
 
 rangeSlider.addEventListener('input', function() {
     let val = rangeSlider.value;
